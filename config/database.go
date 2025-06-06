ac65b4147710b79eacb0214cf3a7fa322e12913a
Infelo <infelo453@gmail.com>
2025-06-06 19:58:26 +0545

    Changes

--- config/database.go
+++ config/database.go
@@ -10,7 +10,6 @@ type DatabaseConfig struct {
 	Host     string
 	Port     string
 	User     string
-	Role     string
 	Password string
 	DBName   string
 	SSLMode  string
@@ -22,7 +21,6 @@ func GetDatabaseConfig() *DatabaseConfig {
 		Host:     getEnv("DB_HOST", "84.247.131.246"),
 		Port:     getEnv("DB_PORT", "5433"),
 		User:     getEnv("DB_USER", "luna"),
-		Role:     getEnv("DB_ROLE", "luna"),
 		Password: getEnv("DB_PASSWORD", "Luna@#$321"),
 		DBName:   getEnv("DB_NAME", "luna_iot"),
 		SSLMode:  getEnv("DB_SSL_MODE", "disable"),
@@ -31,8 +29,8 @@ func GetDatabaseConfig() *DatabaseConfig {
 
 // GetDSN returns the database connection string
 func (c *DatabaseConfig) GetDSN() string {
-	return fmt.Sprintf("host=%s port=%s user=%s role=%s password=%s dbname=%s sslmode=%s",
-		c.Host, c.Port, c.User, c.Role, c.Password, c.DBName, c.SSLMode)
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		c.Host, c.Port, c.User, c.Password, c.DBName, c.SSLMode)
 }
 
 // getEnv gets environment variable with fallback
