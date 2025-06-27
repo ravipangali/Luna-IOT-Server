1f5aa9fac1671550771809c0f9ae27657f13d269
Infelo <infelo453@gmail.com>
2025-06-27 17:40:55 +0545

    Method of delete button

--- internal/http/routes.go
+++ internal/http/routes.go
@@ -57,7 +57,8 @@ func SetupRoutesWithControlController(router *gin.Engine, sharedControlControlle
 			authProtected.POST("/logout", authController.Logout)
 			authProtected.GET("/me", authController.Me)
 			authProtected.POST("/refresh", authController.RefreshToken)
-			authProtected.POST("/delete-account", authController.DeleteAccount)
+			authProtected.GET("/delete-account", authController.DeleteAccount)
+			// authProtected.POST("/delete-account", authController.DeleteAccount)
 		}
 
 		// User routes (admin only for most operations)
