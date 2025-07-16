1fe1a6b3d223a2dd2e1126e002f345d3c43e0a86
Infelo <infelo453@gmail.com>
2025-07-16 12:38:49 +0545

    Firebase issue

--- config/firebase.go
+++ config/firebase.go
@@ -44,22 +44,22 @@ func InitializeFirebase() error {
 	// Try to use environment variables first (if available)
 	projectID := os.Getenv("FIREBASE_PROJECT_ID")
 	if projectID == "" {
-		projectID = "luna-iot-b5cdd" // fallback
+		projectID = "luna-iot-5993f" // fallback
 	}
 
-	// Use hardcoded credentials from your service account file
-	// These are the exact credentials from your firebase-service-account.json
+	// Use hardcoded credentials from your new service account file
+	// These are the exact credentials from your new firebase service account
 	credentials := map[string]interface{}{
 		"type":                        "service_account",
 		"project_id":                  projectID,
-		"private_key_id":              "db5d32a02cc822f776fb829c5ded8ab999d4d9d7",
-		"private_key":                 "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCS6sKIxE3fzazE\nqH/i+DBGbVdAwBTLS2oIyGSUrERB93BdX1PYpykLyxg8tDGN37mUDsRc5aOhc/Ru\nT4gL2yYplw4GXkUEzZaNgmJdPjgsu/w9vmQEN+hUjtGTdgJ2AVilXujFJBxsw/FS\nTBEqLM5cDTbgAE7m5SRdgj/6+jkhOxKk0biPP0caQDSvxcv6LC/XL5oWdwrJ/pNg\nCpLPOY9Um6JPGIiQhdk5IYHGhgl+5IblaAzmmBcvJKXEjLX+GFgiw9rR1gn75fuS\ns4gotBO4XDF3W/UCl2bm0odsXEu0IkHCBFhFsu7LYXgHYNxBGYJ5gg18FwPLkEPr\nV6p3yyrBAgMBAAECggEAH8cel9WaIIgM0CbALrhPPNSGtG83sDdaMpchxlSymPAs\nAk5NxQV3J+FglzTEqTLUobVF/PAA4jnCC6AxRZs72HAfbPo0BJNxdp9WpmOAZBCv\nQS2u1YjAPJX9t98lLiAha/eo8odajJ4fUxU3+z7gzeFf1rjKWEAFCyLSsvcvp0Ob\nX0RLu6awJIz5is+PSI60btKC5+/jxxdIbldg6YSOmiPyW9rXVUfhk0Cjlg4jXIhm\nqs1CYtlD9Bsa6mcujJpSXqjMWysE4K48OXgcJAW6GIn6F20GyeoksYksZfgFvcbn\ncguhg9G72fMpAL1Nvo1Xjlu6fLpV//i/fCYw1BWJlwKBgQDE47Hs9lt2Fbq7p1Lp\nGNtXTxGboagivScWeIT5uKgu/pzszu/C2i0T986/RUXmm4DpNaAGqBwfWHEeqLCg\ne+xp6nDIDQI1PqTxuiRxQLRbpyafssGjln+yGh4L9HN5ZJLr2oqF2tgbRBqMusCq\nwmbycLKulEvIgOjhiX8KeHGTwwKBgQC/BleyrSLGZ0t0nMJYeKmGvZLjS8Wn/CwB\nUnj/Phv9D2sQoa7L1QszBHEAx2p+9ewa0OmaMXOjJttGpVpGQSa9LiUhTqZEOZTa\nf+0/sysx2HKNq5aoPAdn32homAU3TVqiadhDGXdHulSeNLNdFW2Z5CT713+fQzMs\nXl3ZW6KzKwKBgEVDUKFq3SwCYumG6Gzl+KuTPj+AtBRcdHa8ORNceZXmri/EcKYc\neIUwxQOWjAufIs9ntP8CfrosM8c0UsZyMe3ksn49zUwL2JzM/er1dz1S5QyDJwm0\ndQGjnHRaL5FB50mfXOHP5fxZjfl57TNlJjAdo041Dx/e8Y39/7ogOtxfAoGAIIga\n/VHg/zruLcDYlCqQbGLylgT8d1xJvjvmYUmZiKJMkHuIgiwZCSozeHd9mnuVJwf3\nEIxlbh6a71APrLFBwKwQJLj5Nds8j22D4PpJW+bJs3jKYoI+nKD+bfmdwcpJqiku\nbFb06mFAMeU1up+Al9mztrP/hwbxuxejEfY6IhsCgYAVstcBmPpWd6HHGgloDFoh\nYUTSWQB5pUWFwzQjPZMWTMBlI2snmMpECXpR1vWG71HPSD9lZo3lSJApi/Ht2pAb\n2lZ3seVPyU4n/D/MEht25PnQyJPk0NX8b9gvhgQ84DlRFAydcZVWiZqlHhDQtQqx\nNCTpfWr2m6ItzG4UfqnXpg==\n-----END PRIVATE KEY-----\n",
-		"client_email":                "firebase-adminsdk-fbsvc@luna-iot-b5cdd.iam.gserviceaccount.com",
-		"client_id":                   "116176236230697495561",
+		"private_key_id":              "7b2de23547167be850a4c997c7d8c53583377ce8",
+		"private_key":                 "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCm3BG7delgI/u9\n2ObBHe+1lNyCInFddMu6DFs+8t0kSyCcLaJwpzsd0baa5HymtWuu5j9wlasAFAFl\nFdTUpxzvohw2tM1TTNzOBDfAr+/Ibut66KmTEA8WM2+Y4dGmORecq+25+w4l0fOX\n67h/fSpD1YMwnA30BJgdymzXCezNs4pgQPsrx0o/97dWaxRYrZ+Hhsea6GdYwdBo\ncdVKmL2vf2AUD/5ruJ/3N/zJaUNEs3R2UBEYik3TFOKUsSVtiiFXFoGxV0xW5YPR\n//6+ZOd/o20+OitTVlPojhb1EP5mmIduOI3RWO2/VHVtvIL0L+7VTPhb2QZPjnUt\n46llun23AgMBAAECggEAFQL4Bf/Octl/z+OfsIh726/H0hNS7I59QQu2nxynavSS\n7K0fxskoO+m2nEFSzmNgu1Yp0vGQkPfz8li8w50vmvVyUWk/GdLp/dSwzfDZubGj\nXDIuzbgN+PZigoFH4dilTTNRQkQyVIzgUdclv9gbxGL/RtW/5A8tYJhRpa/i7pwN\naHw8W6IMUKd9HuCqXOBXhH1GggMhCEJt2wm7PXzxgsKVa4zxPx82vI2Tuhgox+UE\nIipaK26GkvnDjPyRLyGNZk1f0ntYjv8TbDj3rmsSSfCbxgG+otWS8rlgTN44L7w9\nkBCLVjlslIA2x8qmcbzXK635dPvIqB3OnTo5ca3xYQKBgQDrY9k1V6mpELG5zCaF\nAqE8WN1AIwrT2A1cT1KgH9yr8MOX1n0enPeyFRiKMrAnuecyB72KEYVwFrS5m9XX\nwwc+Oa/jCwqgfanTKaf5SyjPzgds5G/fxUi4jB6epr4+6nqnxL0XCswBSTvK6qm0\n+qQJfGF1VJNRZq728CH1+jkrnwKBgQC1eCVc++7k3qsKbuTVjz+kMhdZ89rAgEmN\n1z9l6eWF1QYZ5wQS0trN9deN+sG0PjRsukR4VJw0HJxX/wlPvlIbWUmPaauaN+sb\naOfVkuoC+3N/bpwDfLd1wVg8KQ/lyTL9PvSQpLGwU17Qms+wqLA+gRDoX3nk2XOv\nctungtn26QKBgQCF1ZmUGKmgNJu4NfjYu2wNMcFqTAJF/JtsFrW10SfYouWymQM+\nuqSinhf7y2IY1Dw9V+VOcTPbTS2oMpBdQsgFeysj/g0mvwwlwZN9zFwB+vSB10g8\nhKEaPKDUN54Hi639YYDZbwwa1xamAtJG0hMeSZfn7BRuveFRCatlfcWvpQKBgCml\nKO3t4yUi9J2wVVOtTC2iUTmTfOAwkLC8dRAuXT4ZZQ0MtyKawRwDDzTGFy4GGIHb\nPVtgD3jmF/sZzElApBcipn8DAR6jNpFTweCBlrKYgij8eVFTjca4WEd2JO/W/Jyh\nlf6bzStp9pho7sDb9ZZiiD7Lqm2aebIJ6d7HaL4BAoGAHBS4o1ymFAS7GyF6Oc58\n9DH8lF/G3iAgZP6qvgt8OPeLk1Xvcd/BIHJf6P2VOYYGQwkrwBsYhL1Y5knEUUsi\nsWEW0HI+zKJbjieuFcKF2th6OYB/EK5k1Zyd2F4Ip3W8gCjyNOp7HhYQ+sv/YLCL\nzxcbvtbrDdQskKZGrFkWlsw=\n-----END PRIVATE KEY-----\n",
+		"client_email":                "firebase-adminsdk-fbsvc@luna-iot-5993f.iam.gserviceaccount.com",
+		"client_id":                   "108105592493412976295",
 		"auth_uri":                    "https://accounts.google.com/o/oauth2/auth",
 		"token_uri":                   "https://oauth2.googleapis.com/token",
 		"auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-		"client_x509_cert_url":        "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40luna-iot-b5cdd.iam.gserviceaccount.com",
+		"client_x509_cert_url":        "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40luna-iot-5993f.iam.gserviceaccount.com",
 		"universe_domain":             "googleapis.com",
 	}
 
